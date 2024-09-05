57beb80ab95ed4617a027da1f501696f9eb653cc
fan ruwen <fruwen@163.com>
2024-09-05 12:51:35 +0000

    remove toAsm

--- backend/riscv/riscvasmemitter.py
+++ backend/riscv/riscvasmemitter.py
@@ -157,7 +157,7 @@ class RiscvSubroutineEmitter():
         self.buf.append(instr)
 
     def emitLabel(self, label: Label):
-        self.buf.append(Riscv.RiscvLabel(label).toAsm([], []))
+        self.buf.append(Riscv.RiscvLabel(label))
 
     
     def emitFunc(self):
